5aaceb917b143a8050281e76a4a79d979d2d7575
KelechiDan <67115071+KelechiDan@users.noreply.github.com>
2020-06-28 03:20:59 +0200

    Add files via upload

--- db_connect.php
+++ db_connect.php
@@ -1,13 +1,27 @@
 <?php
-$host = "localhost";
-$username = "root";
-$password = "";
-$dbase = "blog";
+class Dbh{
 
-// Create connection
-$db = new mysqli($host, $username, $password, $dbase);
+  private $servername;
+  private $username;
+  private $password;
+  private $dbname;
 
-// Check connection
-if ($db->connect_error) {
-  die("Connection failed: " . $db->connect_error);
-} 
+  public function dbConnect () {
+    $this->servername = "localhost";
+    $this->username = "root";
+    $this->password = "";
+    $this->dbname = "blog";
+    $this->charset = "utf8mb4";
+
+   try{
+    $dsn = "mysql:host=".$this->servername.";dbname=".$this->dbname.";charset=".$this->charset;
+    $pdo = new PDO($dsn, $this->username, $this->password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $pdo;
+  } catch (PDOException $e){
+    echo "Connection failed: ".$e->getMessage();
+  }
+  }
+}
+
+?>
---
+++ post_submit.php
@@ -0,0 +1,57 @@
+<?php
+//session_start();
+//include_once ('include/db_connect.php');
+//include_once ('new_post.php');
+
+
+class Post_Submit extends Dbh  {
+
+  private $db;
+
+     //include database connection
+ public function __construct(){
+   $this->db = new Dbh();
+   $this->db = $this->db->dbConnect();
+  }
+//starting a session
+//session_start();
+//include ('db_connect.php');
+//if (!isset($_SESSION["user_id"])){
+//  header('Location: login.php');
+//  exit();
+//}
+ //checking if submit botton has been klicked
+ public function submitPost(){
+
+if (isset($_POST["submit"])){
+    //getting and vallidating data from form
+    $title = $_POST["title"];
+    $body = $_POST["body"];
+    $author = $_POST["author"];
+
+    //$title = $db->real_escape_string($title);
+    //$body = $db->real_escape_string($body);
+    //$author = $db->real_escape_string($author);
+    //$user_id = ""; //$_SESSION["user_id"];
+    $date_1 = date("Y/m/d h:i:sa");
+    $body = htmlentities($body);
+
+    if ($title && $body && $author){
+      // prepare sql and bind parameters
+  $this->db->exec ("INSERT INTO post (title, content, posted, author)
+  VALUES ('$title', '$body', '$date_1', '$author')");
+     //$this->db->exec($sql);
+       //if ($this->db->exec($sql)){
+         echo "Post added.";
+       //} else {
+      //   echo "error";
+       //}
+
+    } else {
+      echo "Missing data.";
+    }
+
+}
+}
+}
+?>
